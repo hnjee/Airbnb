585717e4e1e64ba4006261e4b9221c17e03a941b
candle1503 <61449197+candle1503@users.noreply.github.com>
2020-05-15 16:27:09 +0900

    mapping from airbnb

--- src/main/java/com/airbnb/s1/HomeController.java
+++ src/main/java/com/airbnb/s1/HomeController.java
@@ -33,7 +33,7 @@ public class HomeController {
 		
 		model.addAttribute("serverTime", formattedDate );
 		
-		return "member/memberMyPage";
+		return "place/hostPlaceTest";
 	}
 	
 }
--- src/main/java/com/airbnb/s1/place/PlaceController.java
+++ src/main/java/com/airbnb/s1/place/PlaceController.java
@@ -3,6 +3,7 @@ package com.airbnb.s1.place;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/place/**")
@@ -20,5 +21,13 @@ public class PlaceController {
 	public void placeSelect() throws Exception{
 		
 	}
-
+	
+	@GetMapping("hostPlaceAdd")
+	public ModelAndView hostPlaceAdd(int curpage, ModelAndView mv) throws Exception{
+		System.out.println("enterController");
+		mv.addObject("result", curpage);
+		mv.setViewName("ajaxForm/hostPlace"+curpage);
+		return mv;
+	}
+	
 }
