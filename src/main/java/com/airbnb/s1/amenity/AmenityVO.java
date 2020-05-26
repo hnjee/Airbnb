86fd2b541339e257423d87b41055a9be9e7ec924
hnjee <leehnjee@gmail.com>
2020-05-26 17:16:52 +0900

    d

--- src/main/java/com/airbnb/s1/amenity/AmenityVO.java
+++ src/main/java/com/airbnb/s1/amenity/AmenityVO.java
@@ -1,15 +1,8 @@
 package com.airbnb.s1.amenity;
 
 public class AmenityVO {
-	private String placeNum;
 	private long amenityKind;
 	
-	public String getPlaceNum() {
-		return placeNum;
-	}
-	public void setPlaceNum(String placeNum) {
-		this.placeNum = placeNum;
-	}
 	public long getAmenityKind() {
 		return amenityKind;
 	}
