9561aec2324adb8be74bdf7f05ded3e34d928c74
candle1503 <61449197+candle1503@users.noreply.github.com>
2020-05-28 18:02:59 +0900

    dd

--- src/main/java/com/airbnb/s1/member/MemberController.java
+++ src/main/java/com/airbnb/s1/member/MemberController.java
@@ -200,14 +200,8 @@ public class MemberController {
 	@GetMapping("placeUpdate")
 	public ModelAndView placeUpdate(MemberVO memberVO,PlaceVO placeVO, HttpSession session,ModelAndView mv) throws Exception{
 		memberVO=(MemberVO)session.getAttribute("member");
-
 		List<PlaceVO> placeVOs = placeService.myPlace(memberVO);
-
-		placeVO = placeVOs.get(0);
-
-		System.out.println(placeVO.getPlaceNum());
 			mv.addObject("list", placeVOs);
-
 			mv.setViewName("member/placeUpdate");
 
 			return mv;
